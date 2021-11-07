6c09af73ae3a1c26402ac9b02844177d8866f6be
codefly16 <466885667@qq.com>
2021-11-07 22:04:17 +0800

    feat: base-ui不冲

--- src/base-ui/echart/src/base-echart.vue
+++ src/base-ui/echart/src/base-echart.vue
@@ -5,32 +5,32 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted, defineProps, withDefaults, watchEffect } from 'vue'
-import { EChartsOption } from 'echarts'
-import useEchart from '../hooks/useEchart'
+import { ref, onMounted, defineProps, withDefaults, watchEffect } from 'vue';
+import { EChartsOption } from 'echarts';
+import useEchart from '../hooks/useEchart';
 
 // 定义props
 const props = withDefaults(
   defineProps<{
-    options: EChartsOption
-    width?: string
-    height?: string
+    options: EChartsOption;
+    width?: string;
+    height?: string;
   }>(),
   {
     width: '100%',
     height: '360px'
   }
-)
+);
 
-const echartDivRef = ref<HTMLElement>()
+const echartDivRef = ref<HTMLElement>();
 
 onMounted(() => {
-  const { setOptions } = useEchart(echartDivRef.value!)
+  const { setOptions } = useEchart(echartDivRef.value!);
 
   watchEffect(() => {
-    setOptions(props.options)
-  })
-})
+    setOptions(props.options);
+  });
+});
 </script>
 
 <style scoped></style>
